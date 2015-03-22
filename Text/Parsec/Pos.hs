19a62ae80a64b319ba9af9fbf13b8b1cb2a02a45
Antoine Latter <aslatter@gmail.com>
2015-03-21 20:40:02 -0500

    Remove split-base conditional compilation option.

--- Text/Parsec/Pos.hs
+++ Text/Parsec/Pos.hs
@@ -22,12 +22,8 @@ module Text.Parsec.Pos
     , updatePosChar, updatePosString
     ) where
 
-#ifdef BASE3
-import Data.Generics
-#else
 import Data.Data (Data)
 import Data.Typeable (Typeable)
-#endif
 
 -- < Source positions: a file name, a line and a column
 -- upper left is (1,1)
