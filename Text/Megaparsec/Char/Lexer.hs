c534ec798935318913dc69901a3c76e21c8e5c6c
Mark Karpov <markkarpov92@gmail.com>
2018-03-21 17:25:26 +0700

    Enable more warnings (#285)

--- Text/Megaparsec/Char/Lexer.hs
+++ Text/Megaparsec/Char/Lexer.hs
@@ -397,10 +397,10 @@ charLiteral :: (MonadParsec e s m, Token s ~ Char) => m Char
 charLiteral = label "literal character" $ do
   -- The @~@ is needed to avoid requiring a MonadFail constraint,
   -- and we do know that r will be non-empty if count' succeeds.
-  ~r@(x:_) <- lookAhead $ count' 1 8 C.anyChar
+  r <- lookAhead (count' 1 8 C.anyChar)
   case listToMaybe (Char.readLitChar r) of
-    Just (c, r') -> count (length r - length r') C.anyChar >> return c
-    Nothing      -> unexpected (Tokens (x:|[]))
+    Just (c, r') -> c <$ skipCount (length r - length r') C.anyChar
+    Nothing      -> unexpected (Tokens (head r:|[]))
 {-# INLINEABLE charLiteral #-}
 
 ----------------------------------------------------------------------------
